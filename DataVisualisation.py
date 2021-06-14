8ec622d4a9a24b930a3a00511b03156028291a9c
Shreyas Mocherla <shreyas@shreyasm.com>
2021-06-14 12:21:19 +0530

    Update DataVisualisation.py

--- DataVisualisation.py
+++ DataVisualisation.py
@@ -1,7 +1,7 @@
 import plotly.express as px
 import pandas as pd
 
-vacc = pd.read_csv('/home/shreyas/Downloads/vaccination-data.csv')
+vacc = pd.read_csv('Path of the downbloaded vaccination csv file to be inserted here. Be sure to download it from the official WHO website as the column headings must remain intact.')
 
 gropvacc = vacc.groupby(["COUNTRY"])[["TOTAL_VACCINATIONS"]].mean().reset_index()
 fig = px.bar(gropvacc[["COUNTRY", "TOTAL_VACCINATIONS"]].sort_values('TOTAL_VACCINATIONS', ascending=False),
